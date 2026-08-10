990677e928f3efe93be558e316c5865ce0d7a369
Nikolay Cherkashin <nikkirche@hotmail.com>
2026-08-10 22:34:18 +0300

    fixed folly

--- syscall_intercept/pthread.cpp
+++ syscall_intercept/pthread.cpp
@@ -1,26 +1,36 @@
 
 #include <pthread.h>
 
+#include <cassert>
+#include <dlfcn.h>
 #include <utility>
 
 #include "runtime/include/blocking_primitives.h"
 #include "runtime/include/coro_ctx_guard.h"
 #include "runtime/include/os_simulator.h"
 
-#define PTHREAD_MOCK_INIT_ROUTINE                                     \
-  if (!ltest_initialized) {                                           \
-    return 0;                                                         \
-  }                                                                   \
-  assert(ltest_coro_ctx && "should be called only from tested code"); \
+[[nodiscard]] bool ShouldUseMock() {
+  return ltest_initialized && ltest_coro_ctx;
+}
+
+#define PTHREAD_MOCK_INIT_ROUTINE(name, ...)                              \
+  if (!ShouldUseMock()) {                                                 \
+    static decltype(&name) real_fn = nullptr;                             \
+    if (!real_fn) {                                                       \
+      reinterpret_cast<void*&>(real_fn) = dlsym(RTLD_NEXT, #name);        \
+      assert(real_fn);                                                    \
+    }                                                                     \
+    return real_fn(__VA_ARGS__);                                          \
+  }                                                                       \
   ltest::SchedCtxGuard guard;
 
 extern int pthread_create(pthread_t *__restrict __newthread,
                           const pthread_attr_t *__restrict __attr,
                           void *(*__start_routine)(void *),
                           void *__restrict __arg) __THROWNL {
-  assert(ltest_coro_ctx);
+  PTHREAD_MOCK_INIT_ROUTINE(pthread_create, __newthread, __attr,
+                            __start_routine, __arg)
   assert(!thread_info.has_value());
-  ltest::SchedCtxGuard guard;
   std::string name = "NewThread";
   thread_info.emplace(CreatedThreadInfo(
       [__start_routine, __arg](void *) {
@@ -38,9 +48,8 @@ extern int pthread_create(pthread_t *__restrict __newthread,
 }
 
 int pthread_join(pthread_t __th, void **__thread_return) {
-  assert(ltest_coro_ctx);
+  PTHREAD_MOCK_INIT_ROUTINE(pthread_join, __th, __thread_return)
   assert(!thread_info.has_value());
-  ltest::SchedCtxGuard guard;
   thread_info.emplace(JoinThreadInfo(__th, __thread_return));
   {
     ltest::CoroCtxGuard coro_guard;
@@ -50,8 +59,7 @@ int pthread_join(pthread_t __th, void **__thread_return) {
 }
 
 extern void pthread_exit(void *__retval) __attribute__((__noreturn__)) {
-  assert(ltest_coro_ctx);
-  ltest::SchedCtxGuard guard;
+  PTHREAD_MOCK_INIT_ROUTINE(pthread_exit, __retval)
   this_coro->TerminateWith(ValueWrapper(__retval, GetDefaultCompator<void *>(),
                                         &GetThreadResultToString));
   {
@@ -91,24 +99,24 @@ static decltype(mutexes)::mapped_type &FindMutex(pthread_mutex_t *__mutex,
 }
 extern int pthread_mutex_init(pthread_mutex_t *__mutex,
                               const pthread_mutexattr_t *__mutexattr) __THROW {
-  assert(ltest_coro_ctx);
-  ltest::SchedCtxGuard guard;
+  PTHREAD_MOCK_INIT_ROUTINE(pthread_mutex_init, __mutex, __mutexattr)
   return 0;
 }
 
 /* Destroy a mutex.  */
 extern int pthread_mutex_destroy(pthread_mutex_t *__mutex) __THROW {
-  PTHREAD_MOCK_INIT_ROUTINE
   auto it = mutexes.find(__mutex);
   if (it != mutexes.end()) {
     mutexes.erase(it);
+    return 0;
   }
+  PTHREAD_MOCK_INIT_ROUTINE(pthread_mutex_destroy, __mutex)
   return 0;
 }
 
 /* Try locking a mutex.  */
 extern int pthread_mutex_trylock(pthread_mutex_t *__mutex) __THROWNL {
-  PTHREAD_MOCK_INIT_ROUTINE
+  PTHREAD_MOCK_INIT_ROUTINE(pthread_mutex_trylock, __mutex)
   auto &it = InsertMutex(__mutex);
   return std::visit(Overloads{[](auto &a) {
                       bool res = a.try_lock();
@@ -122,7 +130,7 @@ extern int pthread_mutex_trylock(pthread_mutex_t *__mutex) __THROWNL {
 
 /* Lock a mutex.  */
 extern int pthread_mutex_lock(pthread_mutex_t *__mutex) __THROWNL {
-  PTHREAD_MOCK_INIT_ROUTINE
+  PTHREAD_MOCK_INIT_ROUTINE(pthread_mutex_lock, __mutex)
   auto &it = InsertMutex(__mutex);
   return std::visit(Overloads{[](auto &a) {
                       a.lock();
@@ -133,7 +141,7 @@ extern int pthread_mutex_lock(pthread_mutex_t *__mutex) __THROWNL {
 
 /* Unlock a mutex.  */
 extern int pthread_mutex_unlock(pthread_mutex_t *__mutex) __THROWNL {
-  PTHREAD_MOCK_INIT_ROUTINE
+  PTHREAD_MOCK_INIT_ROUTINE(pthread_mutex_unlock, __mutex)
   auto &it = FindMutex(__mutex, "unlock called before lock");
   return std::visit(Overloads{[](auto &a) {
                       a.unlock();
@@ -156,28 +164,30 @@ static decltype(cond_variables)::mapped_type &InsertCond(pthread_cond_t *cond) {
 extern int pthread_cond_init(
     pthread_cond_t *__restrict __cond,
     const pthread_condattr_t *__restrict __cond_attr) __THROW {
+  PTHREAD_MOCK_INIT_ROUTINE(pthread_cond_init, __cond, __cond_attr)
   return 0;
 }
 
 /* Destroy condition variable COND.  */
 extern int pthread_cond_destroy(pthread_cond_t *__cond) __THROW {
-  PTHREAD_MOCK_INIT_ROUTINE
   auto it = cond_variables.find(__cond);
   if (it != cond_variables.end()) {
     cond_variables.erase(it);
+    return 0;
   }
+  PTHREAD_MOCK_INIT_ROUTINE(pthread_cond_destroy, __cond)
   return 0;
 }
 
 extern int pthread_cond_signal(pthread_cond_t *__cond) __THROWNL {
-  PTHREAD_MOCK_INIT_ROUTINE
+  PTHREAD_MOCK_INIT_ROUTINE(pthread_cond_signal, __cond)
   auto &c = InsertCond(__cond);
   c.notify_one();
   return 0;
 }
 
 extern int pthread_cond_broadcast(pthread_cond_t *__cond) __THROWNL {
-  PTHREAD_MOCK_INIT_ROUTINE
+  PTHREAD_MOCK_INIT_ROUTINE(pthread_cond_broadcast, __cond)
   auto &c = InsertCond(__cond);
   c.notify_all();
   return 0;
@@ -185,7 +195,7 @@ extern int pthread_cond_broadcast(pthread_cond_t *__cond) __THROWNL {
 
 extern int pthread_cond_wait(pthread_cond_t *__restrict __cond,
                              pthread_mutex_t *__restrict __mutex) {
-  PTHREAD_MOCK_INIT_ROUTINE
+  PTHREAD_MOCK_INIT_ROUTINE(pthread_cond_wait, __cond, __mutex)
   auto &m = InsertMutex(__mutex);
   auto &c = InsertCond(__cond);
   // todo - allow other mutexes
@@ -205,23 +215,24 @@ static decltype(shared_mutexes)::mapped_type &InsertSharedLock(
 extern int pthread_rwlock_init(
     pthread_rwlock_t *__restrict __rwlock,
     const pthread_rwlockattr_t *__restrict __attr) __THROW {
-  PTHREAD_MOCK_INIT_ROUTINE
+  PTHREAD_MOCK_INIT_ROUTINE(pthread_rwlock_init, __rwlock, __attr)
   return 0;
 }
 
 /* Destroy read-write lock RWLOCK.  */
 extern int pthread_rwlock_destroy(pthread_rwlock_t *__rwlock) __THROW {
-  PTHREAD_MOCK_INIT_ROUTINE
   auto it = shared_mutexes.find(__rwlock);
   if (it != shared_mutexes.end()) {
     shared_mutexes.erase(it);
+    return 0;
   }
+  PTHREAD_MOCK_INIT_ROUTINE(pthread_rwlock_destroy, __rwlock)
   return 0;
 }
 
 /* Acquire read lock for RWLOCK.  */
 extern int pthread_rwlock_rdlock(pthread_rwlock_t *__rwlock) {
-  PTHREAD_MOCK_INIT_ROUTINE
+  PTHREAD_MOCK_INIT_ROUTINE(pthread_rwlock_rdlock, __rwlock)
   auto &it = InsertSharedLock(__rwlock);
   return std::visit(Overloads{[](auto &a) {
                       a.lock_shared();
@@ -232,14 +243,14 @@ extern int pthread_rwlock_rdlock(pthread_rwlock_t *__rwlock) {
 
 /* Try to acquire read lock for RWLOCK.  */
 extern int pthread_rwlock_tryrdlock(pthread_rwlock_t *__rwlock) {
-  PTHREAD_MOCK_INIT_ROUTINE
+  PTHREAD_MOCK_INIT_ROUTINE(pthread_rwlock_tryrdlock, __rwlock)
   auto &it = InsertSharedLock(__rwlock);
   throw std::runtime_error("not implemented");
 }
 
 /* Acquire write lock for RWLOCK.  */
 extern int pthread_rwlock_wrlock(pthread_rwlock_t *__rwlock) {
-  PTHREAD_MOCK_INIT_ROUTINE
+  PTHREAD_MOCK_INIT_ROUTINE(pthread_rwlock_wrlock, __rwlock)
   auto &it = InsertSharedLock(__rwlock);
   return std::visit(Overloads{[](auto &a) {
                       a.lock();
@@ -250,13 +261,13 @@ extern int pthread_rwlock_wrlock(pthread_rwlock_t *__rwlock) {
 
 /* Try to acquire write lock for RWLOCK.  */
 extern int pthread_rwlock_trywrlock(pthread_rwlock_t *__rwlock) {
-  PTHREAD_MOCK_INIT_ROUTINE
+  PTHREAD_MOCK_INIT_ROUTINE(pthread_rwlock_trywrlock, __rwlock)
   throw std::runtime_error("not implemented");
 }
 
 /* Unlock RWLOCK.  */
 extern int pthread_rwlock_unlock(pthread_rwlock_t *__rwlock) __THROWNL {
-  PTHREAD_MOCK_INIT_ROUTINE
+  PTHREAD_MOCK_INIT_ROUTINE(pthread_rwlock_unlock, __rwlock)
   auto &it = InsertSharedLock(__rwlock);
   return std::visit(Overloads{[](auto &a) {
                       a.unlock();
